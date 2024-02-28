73d46355a7ea5f3a067522a3b78d74636d2186b6
Karolina Kowalczyk <karolina.a.kowalczyk@comarch.com>
2024-02-28 14:47:18 +0100

    animations and mat-tab-group transition added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, ViewChild } from '@angular/core';
+import { MatTabHeaderPosition } from '@angular/material/tabs';
 
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0,
+  }),
+  animate('0.6s ease-in', style({ opacity: 1 })),
+]);
+const fadeIn = trigger('fadeIn', [enterTransition]);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  animations: [fadeIn],
 })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild('appContainer') appContainer: HTMLElement;
+  currentTabIndex = 0;
+  position = 'below' as MatTabHeaderPosition;
+
+  tabChanged($event: number): void {
+    this.currentTabIndex = $event;
+    this.currentTabIndex === 0
+      ? (this.position = 'below')
+      : (this.position = 'above');
+  }
+}
